8543b4b08b64b00cd931c7aaf600111b61632556
Honza Böhm <jan.bohm@infis.cz>
2026-09-03 13:13:52 +0200

    Changed output message

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Nothing to see here.");
+        System.out.println("Change");
+        System.out.println("New block");
     }
 }
