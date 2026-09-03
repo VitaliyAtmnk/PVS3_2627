a51110bb09084127b5832c2c82fbd398eb1ae100
matej.breu <matej.breu@infis.cz>
2026-09-03 13:12:48 +0200

    changed output

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
+import fileworks.DataImport;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Nothing to see here.");
+        System.out.println("Díky za turnaj");
+
     }
 }
