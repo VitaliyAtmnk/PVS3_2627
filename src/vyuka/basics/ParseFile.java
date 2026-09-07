eee65fec8f7fee9b197eee558e984b05b339b22b
Ondřej Wislocki <ondrej.wislocki@infis.cz>
2026-09-07 15:23:29 +0200

    7.9

--- src/vyuka/basics/ParseFile.java
+++ src/vyuka/basics/ParseFile.java
@@ -1,4 +1,26 @@
 package vyuka.basics;
 
+import fileworks.DataImport;
+
 public class ParseFile {
+    public static void main(String[] args) {
+        String path = "data/countries.txt";
+        DataImport di = new DataImport(path);
+
+
+
+        while(di.hasNext()){
+            String[] line = di.readLine().split(";");
+            Country c = new Country(line[0], line[1], Double.parseDouble(line[2]), Double.parseDouble(line[3]));
+            c.printCountry();
+            System.out.println(c.toString());
+        }
+
+
+
+
+
+
+        di.finishImport();
+    }
 }
